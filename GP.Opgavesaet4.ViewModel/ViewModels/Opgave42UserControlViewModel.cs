38b68c71b1da7d03a27482002b12c12c9e532bd2
Klaus Mark <klaus@11k.dk>
2016-04-20 11:12:50 +0200

    Opgave 4.2

--- GP.Opgavesaet4.ViewModel/ViewModels/Opgave42UserControlViewModel.cs
+++ GP.Opgavesaet4.ViewModel/ViewModels/Opgave42UserControlViewModel.cs
@@ -1,8 +1,28 @@
 ﻿using GP.Opgavesaet4.ViewModel.BaseClasses;
+using GP.Opgavesaet4.ViewModel.Helpers;
 
 namespace GP.Opgavesaet4.ViewModel.ViewModels
 {
     public class Opgave42UserControlViewModel : NotifyBase
     {
+        private readonly Dice _dice = new Dice();
+
+        public Opgave42UserControlViewModel()
+        {
+            ThrowDiceCommand = new DelegateCommand(ThrowDice);
+        }
+
+        public DelegateCommand ThrowDiceCommand { get; }
+        public void ThrowDice()
+        {
+            _dice.Throw();
+            LastDiceResult = _dice.show();
+        }
+
+        public int LastDiceResult
+        {
+            get { return GetField<int>(); }
+            set { SetField(value); }
+        }
     }
 }
