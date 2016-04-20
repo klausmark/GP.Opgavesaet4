277b75bb8077a38f66c5e39b783c02d26617a0fc
Klaus Mark <klaus@11k.dk>
2016-04-20 11:37:32 +0200

    Opgave 4.3

---
+++ GP.Opgavesaet4.Model/DiceExtensions.cs
@@ -0,0 +1,14 @@
+﻿public static class DiceExtensions
+{
+    public static int[] ThrowTimes(this Dice dice, int numberOfThrows)
+    {
+        if (numberOfThrows < 1) return new int[0];
+        var throws = new int[numberOfThrows];
+        for (var i = 0; i < numberOfThrows; i++)
+        {
+            dice.Throw();
+            throws[i] = dice.show();
+        }
+        return throws;
+    }
+}
--- GP.Opgavesaet4.ViewModel/ViewModels/Opgave43UserControlViewModel.cs
+++ GP.Opgavesaet4.ViewModel/ViewModels/Opgave43UserControlViewModel.cs
@@ -4,5 +4,27 @@ namespace GP.Opgavesaet4.ViewModel.ViewModels
 {
     public class Opgave43UserControlViewModel : NotifyBase
     {
+        private readonly Dice _dice = new Dice();
+        public int NumberOfThrows
+        {
+            get { return GetField<int>(); }
+            set
+            {
+                SetField(value);
+                ThrowDice();
+            }
+        }
+
+        private void ThrowDice()
+        {
+            Throws = _dice.ThrowTimes(NumberOfThrows);
+        }
+
+        public int[] Throws
+        {
+            get { return GetField<int[]>(); }
+            set { SetField(value); }
+        }
+
     }
 }
