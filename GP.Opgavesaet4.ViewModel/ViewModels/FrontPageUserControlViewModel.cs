14a47e89162f3b0cc1044a725d5d1a6c96cc9e7e
Klaus Mark <klaus@11k.dk>
2016-04-20 11:47:47 +0200

    Last befor handover

--- GP.Opgavesaet4.ViewModel/ViewModels/FrontPageUserControlViewModel.cs
+++ GP.Opgavesaet4.ViewModel/ViewModels/FrontPageUserControlViewModel.cs
@@ -1,8 +1,10 @@
-﻿using GP.Opgavesaet4.ViewModel.BaseClasses;
+﻿using System;
+using GP.Opgavesaet4.ViewModel.BaseClasses;
 
 namespace GP.Opgavesaet4.ViewModel.ViewModels
 {
     public class FrontPageUserControlViewModel : NotifyBase
     {
+        public string Tekst { get; } = $"Mine løsninger på Opgavesæt4 dog uden 4.4 grundet mangel på tid.{Environment.NewLine}Jeg håber det faktum at jeg har lavet dem rigtig godt opvejer manglen på 4.4";
     }
 }
